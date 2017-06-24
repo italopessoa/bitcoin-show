e0abf63ed92b4e951e1c068e17aa861e5061e21a
Italo Pessoa <italo.pessoa@hotmail.com>
2017-06-24 15:17:53 -0300

    atom web design configured. may have errors

--- bitcoin-show/src/index.js
+++ bitcoin-show/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import App from './components/App';
+import registerServiceWorker from './components/registerServiceWorker';
+import './components/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
