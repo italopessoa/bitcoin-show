08b65cf8ca6cca8c4fecdf22eae289f1739a8a9f
Italo Pessoa <italo.pessoa@hotmail.com>
2017-06-25 16:42:00 -0300

    add action to select an answer

--- bitcoin-show/src/components/atoms/CardPanel/CardPanel.jsx
+++ bitcoin-show/src/components/atoms/CardPanel/CardPanel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardPanel = (props) =>{
     return(
-        <div className= {"card-panel "+props.className} id={props.id}>
+        <div className= {"card-panel "+props.className} id={props.id} onClick={() => props.onSelectHandler(props.itemIndex)}>
             <div>
             {props.content}
             </div>
--- bitcoin-show/src/components/organisms/QuestionOptionsPanel/QuestionOptionsPanel.jsx
+++ bitcoin-show/src/components/organisms/QuestionOptionsPanel/QuestionOptionsPanel.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
+
 import CardPanel from '../../atoms/CardPanel/CardPanel.jsx'
-const QuestionOptionsPanel = (props) =>{
+
+class QuestionOptionsPanel extends Component{
+  constructor(props){
+    super(props);
+    this.state = {selectedIndex: -1}
+  }
+  selectCardHandler = (index) =>{
+    this.setState({selectedIndex : index});
+  }
+  render(){
     return (
         <div>
-         {
-             props.options.map((option,index) =>{
-                 return(<CardPanel key={index} className="red white-text touchable selected-answer waves-effect waves-light" content={option} />);
-             })
-         }
+          {
+            this.props.options.map((option,index) =>{
+              return(<CardPanel key={index} onSelectHandler={this.selectCardHandler} itemIndex={index} 
+                    className={'red white-text touchable waves-effect waves-light ' + (this.state.selectedIndex === index ? 'selected-answer' : '')} content={option} />);
+            })
+          }
         </div>
-    )
+      )
+  }
 }
 
 export default QuestionOptionsPanel;
