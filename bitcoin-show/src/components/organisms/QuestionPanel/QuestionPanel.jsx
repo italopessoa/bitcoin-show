ded0811043ed5051092f9d3fdeafbc7c1a54e91a
Italo Pessoa <italo.pessoa@hotmail.com>
2017-06-26 19:00:37 -0300

    add CardPanel component at QuestionPanel.jsx

--- bitcoin-show/src/components/organisms/QuestionPanel/QuestionPanel.jsx
+++ bitcoin-show/src/components/organisms/QuestionPanel/QuestionPanel.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import QuestionTitlePanel from '../../molecules/QuestionTitlePanel/QuestionTitlePanel.jsx'
 import QuestionOptionsPanel from '../../molecules/QuestionOptionsPanel/QuestionOptionsPanel.jsx'
+import CardPanel from '../../atoms/CardPanel/CardPanel.jsx'
 import './QuestionPanel.css'
 
 const QuestionPanel = () =>{
   return(
-    <div className="card-panel blue darken-3 zero-padding-left">
-      <div className="row">
-        <div className="col m12 zero-padding-left">
-          <QuestionTitlePanel />
+    <CardPanel className="card-panel blue darken-3 zero-padding-left"
+      content={
+        <div>
+          <div className="row">
+            <div className="col m12 zero-padding-left">
+              <QuestionTitlePanel />
+            </div>
+          </div>
+          <div className="row ">
+            <div className="col m9 s11">
+              <QuestionOptionsPanel options={['Karl Marx','Tio patinhas','Chapolin colorado','Chespirito']} />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="row ">
-        <div className="col m9 s11">
-          <QuestionOptionsPanel options={['Karl Marx','Tio patinhas','Chapolin colorado','Chespirito']} />
-        </div>
-      </div>
-    </div>
+      }
+    />
   );
 }
 
