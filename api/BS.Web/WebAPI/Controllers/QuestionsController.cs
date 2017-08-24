ee9d86f51b7d05387b955f1282cfdaf391cff441
Italo Pessoa <italo.pessoa@hotmail.com>
2017-08-23 21:49:59 -0300

    add method to return award values, and level

--- api/BS.Web/WebAPI/Controllers/QuestionsController.cs
+++ api/BS.Web/WebAPI/Controllers/QuestionsController.cs
@@ -20,6 +20,15 @@ namespace WebAPI.Controllers
         Hard = 2
     }
 
+    public class Award
+    {
+        public byte Number { get; set; }
+        public int Right { get; set; }
+        public int Stop { get; set; }
+        public int Wrong { get; set; }
+        public LevelEnum Level { get; set; }
+    }
+
     //https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger
     //https://docs.microsoft.com/en-us/azure/app-service-api/app-service-api-dotnet-swashbuckle-customize
 
@@ -43,19 +52,67 @@ namespace WebAPI.Controllers
         public IActionResult Get(LevelEnum level = LevelEnum.Easy)
         {
             return Ok(
-                new { question = this.GenerateRandomQuestion() }
+                new { question = this.GenerateRandomQuestion(level) }
             );
         }
 
-        private Question GenerateRandomQuestion()
+        [HttpGet("awards")]
+        public IActionResult GetAwards()
         {
-            Question a = new Question { Id = 0,Level='e',Text = $"Question {DateTime.Now.ToLongDateString()}?"};
+            List<Award> awards = new List<Award>
+            {
+                new Award {Number= 1,Right= 500,Stop= 0,Wrong= 0},
+                new Award {Number= 2,Right= 600,Stop= 500,Wrong= 250},
+                new Award {Number= 3,Right= 700,Stop= 600,Wrong= 300},
+                new Award {Number= 4,Right= 800,Stop= 750,Wrong= 350},
+                new Award {Number= 5,Right= 900,Stop= 800,Wrong= 400},
+                new Award {Number= 6,Right= 1000,Stop= 900,Wrong= 450},
+                new Award {Number= 7,Right= 2000,Stop= 1000,Wrong= 500},
+                new Award {Number= 8,Right= 3000,Stop= 2000,Wrong= 1000},
+                new Award {Number= 9,Right= 4000,Stop= 3000,Wrong= 1500},
+                new Award {Number= 10,Right= 5000,Stop= 4000,Wrong= 2000},
+                new Award {Number= 11,Right= 6000,Stop= 5000,Wrong= 2500},
+                new Award {Number= 12,Right= 10000,Stop= 6000,Wrong= 3000},
+                new Award {Number= 13,Right= 20000,Stop= 10000,Wrong= 5000},
+                new Award {Number= 14,Right= 30000,Stop= 20000,Wrong= 10000},
+                new Award {Number= 15,Right= 40000,Stop= 30000,Wrong= 15000},
+                new Award {Number= 16,Right= 50000,Stop= 40000,Wrong= 20000},
+                new Award {Number= 17,Right= 60000,Stop= 50000,Wrong= 25000},
+                new Award {Number= 18,Right= 100000,Stop= 60000,Wrong= 30000},
+                new Award {Number= 19,Right= 200000,Stop= 100000,Wrong= 50000},
+                new Award {Number= 20,Right= 300000,Stop= 200000,Wrong= 100000},
+                new Award {Number= 21,Right= 400000,Stop= 300000,Wrong= 150000},
+                new Award {Number= 22,Right= 500000,Stop= 400000,Wrong= 200000},
+                new Award {Number= 23,Right= 600000,Stop= 500000,Wrong= 250000},
+                new Award {Number= 24,Right= 1000000,Stop= 600000,Wrong= 0}
+            };
+
+            return Ok(new { awards = awards });
+        }
+
+        private Question GenerateRandomQuestion(LevelEnum levelEnum)
+        {
+            string level = String.Empty;
+            switch (levelEnum)
+            {
+                case LevelEnum.Easy:
+                    level = "Easy";
+                    break;
+                case LevelEnum.Medium:
+                    level = "Medium";
+                    break;
+                case LevelEnum.Hard:
+                    level = "Hard";
+                    break;
+            }
+
+            Question a = new Question { Id = 0, Level = 'e', Text = $"{level} {DateTime.Now.ToString()}?" };
             Option[] ops = new Option[4];
             for (byte i = 1; i < 5; i++)
             {
-                ops[i-1]= new Option {Number=i, Text=Guid.NewGuid().ToString()};
-                a.Answer = ops[i-1];
+                ops[i - 1] = new Option { Number = i, Text = Guid.NewGuid().ToString() };
             }
+            a.Answer = ops[new Random().Next(0, 4)];
             a.Options = ops;
             return a;
         }
@@ -78,4 +135,6 @@ namespace WebAPI.Controllers
         //{
         //}
     }
+
+
 }
